8f5d7e11e26ec0181bdbf5eccaece537f3b58b88
Calos Chacin <chacin.carlos9798@gmail.com>
2019-01-30 17:38:33 -0500

    Added module for hal_wrapper

---
+++ build/Release/.deps/Release/obj.target/addon/hal_wrapper/drivers/everloop.o.d
@@ -0,0 +1,67 @@
+cmd_Release/obj.target/addon/hal_wrapper/drivers/everloop.o := g++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pi/.node-gyp/11.8.0/include/node -I/home/pi/.node-gyp/11.8.0/src -I/home/pi/.node-gyp/11.8.0/deps/openssl/config -I/home/pi/.node-gyp/11.8.0/deps/openssl/openssl/include -I/home/pi/.node-gyp/11.8.0/deps/uv/include -I/home/pi/.node-gyp/11.8.0/deps/zlib -I/home/pi/.node-gyp/11.8.0/deps/v8/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -MMD -MF ./Release/.deps/Release/obj.target/addon/hal_wrapper/drivers/everloop.o.d.raw   -c -o Release/obj.target/addon/hal_wrapper/drivers/everloop.o ../hal_wrapper/drivers/everloop.cpp
+Release/obj.target/addon/hal_wrapper/drivers/everloop.o: \
+ ../hal_wrapper/drivers/everloop.cpp ../node_modules/nan/nan.h \
+ /home/pi/.node-gyp/11.8.0/include/node/node_version.h \
+ /home/pi/.node-gyp/11.8.0/include/node/uv.h \
+ /home/pi/.node-gyp/11.8.0/include/node/uv/errno.h \
+ /home/pi/.node-gyp/11.8.0/include/node/uv/version.h \
+ /home/pi/.node-gyp/11.8.0/include/node/uv/unix.h \
+ /home/pi/.node-gyp/11.8.0/include/node/uv/threadpool.h \
+ /home/pi/.node-gyp/11.8.0/include/node/uv/linux.h \
+ /home/pi/.node-gyp/11.8.0/include/node/node.h \
+ /home/pi/.node-gyp/11.8.0/include/node/v8.h \
+ /home/pi/.node-gyp/11.8.0/include/node/v8-version.h \
+ /home/pi/.node-gyp/11.8.0/include/node/v8config.h \
+ /home/pi/.node-gyp/11.8.0/include/node/v8-platform.h \
+ /home/pi/.node-gyp/11.8.0/include/node/node_version.h \
+ /home/pi/.node-gyp/11.8.0/include/node/node_buffer.h \
+ /home/pi/.node-gyp/11.8.0/include/node/node.h \
+ /home/pi/.node-gyp/11.8.0/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_private.h \
+ ../node_modules/nan/nan_typedarray_contents.h \
+ ../node_modules/nan/nan_json.h ../hal_wrapper/drivers/everloop.h \
+ ../hal_wrapper/drivers/../matrix.h \
+ /home/pi/.node-gyp/11.8.0/include/node/v8.h
+../hal_wrapper/drivers/everloop.cpp:
+../node_modules/nan/nan.h:
+/home/pi/.node-gyp/11.8.0/include/node/node_version.h:
+/home/pi/.node-gyp/11.8.0/include/node/uv.h:
+/home/pi/.node-gyp/11.8.0/include/node/uv/errno.h:
+/home/pi/.node-gyp/11.8.0/include/node/uv/version.h:
+/home/pi/.node-gyp/11.8.0/include/node/uv/unix.h:
+/home/pi/.node-gyp/11.8.0/include/node/uv/threadpool.h:
+/home/pi/.node-gyp/11.8.0/include/node/uv/linux.h:
+/home/pi/.node-gyp/11.8.0/include/node/node.h:
+/home/pi/.node-gyp/11.8.0/include/node/v8.h:
+/home/pi/.node-gyp/11.8.0/include/node/v8-version.h:
+/home/pi/.node-gyp/11.8.0/include/node/v8config.h:
+/home/pi/.node-gyp/11.8.0/include/node/v8-platform.h:
+/home/pi/.node-gyp/11.8.0/include/node/node_version.h:
+/home/pi/.node-gyp/11.8.0/include/node/node_buffer.h:
+/home/pi/.node-gyp/11.8.0/include/node/node.h:
+/home/pi/.node-gyp/11.8.0/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_private.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../node_modules/nan/nan_json.h:
+../hal_wrapper/drivers/everloop.h:
+../hal_wrapper/drivers/../matrix.h:
+/home/pi/.node-gyp/11.8.0/include/node/v8.h:
--- index.js
+++ index.js
@@ -1,115 +1,18 @@
-// testing exposed c++
-const matrix = require('./build/Release/addon');
+matrixLite = require('./build/Release/addon');
+led = require('./lib/led.js');
 
-// Returns & logs a string (test function)
-// console.log(matrix);
+// - final module exports
+function exported(){
+  var drivers = {
+    "led":      led,
+    "gpio":     matrixLite.gpio(),
+    "imu":      matrixLite.imu(),
+    "humidity": matrixLite.humidity(),
+    "pressure": matrixLite.pressure(),
+    "uv":       matrixLite.uv()
+  }
 
-/////////////////
-// LED EXAMPLE \\
-var everloop = matrix.led() // get LED object (everloop is the ring of LEDs)
-var leds = []; // array representing each LED
-
-// For each LED
-for (i = 0; i < everloop.length; i++){
-  // Push new color configuration (0-255)
-  leds.push({
-    red: 1,
-    blue: 1,
-    green: 0
-  });
+  return drivers;
 }
-// Write configuration to everloop
-everloop.set(leds);
 
-///////////////////////////////////
-// GPIO DIGITAL SET/READ EXAMPLE \\
-// var gpio = matrix.gpio(); // get GPIO object
-// // Set pin 4 mode (0=INPUT or 1=OUTPUT)
-// gpio.setMode(4,1);
-
-// // Toggle pin value ON/OFF
-// gpio.setDigital(4,1)// set initial value
-// var counter = 0;
-// // toggle value every second
-// setInterval(function(){
-//   if (counter%2 === 0) {gpio.setDigital(4,0);}
-//   else if (counter%2 === 1) {gpio.setDigital(4,1);}
-//   counter++;
-// },1000);
-
-// // Log pin state
-// setInterval(function(){
-//   if(gpio.readDigital(4)){console.log("PIN 4 is ON");}
-//   else {console.log("PIN 4 is OFF");}
-// },50)
-
-////////////////////////////
-// GPIO PWM SERVO ANGLE EXAMPLE \\
-// var gpio = matrix.gpio(); // get GPIO object
-// gpio.setMode(4,1);// set pin 0 to output
-// gpio.setFunction(4, 1);// set pin 0 to PWM
-
-// var percentage = 0.8;
-// gpio.setServoAngle(179,percentage,4)
-// setTimeout(function(){
-//   gpio.setServoAngle(1,percentage,4)
-// },2000);
-// float angle, float min_pulse_ms, int pin (0-15)
-// var angle = 0;
-// setInterval(function(){
-//   if(angle < 180){
-//     console.log(angle);
-//     gpio.setServoAngle(angle,0.5,0)// set servo angle
-//     angle ++;
-//   }
-// },100);
-
-
-//////////////////////
-// GPIO PWM EXAMPLE \\
-// var gpio = matrix.gpio(); // get GPIO object
-// gpio.setMode(0, 1);// set pin 0 to output
-// gpio.setFunction(0, 1);// set pin 0 to PWM
-// var percentage = 0;
-// setInterval(function(){
-//   console.log(percentage);
-//   // Parameters: float frequency, float percentage, int pin (0-15)
-//   gpio.setPWM(50, percentage, 0)// set pwm for pin 0
-//   percentage += 0.2;
-// },500);
-
-/////////////////
-// IMU EXAMPLE \\
-// var imu = matrix.imu(); // get IMU object
-
-// // get & refresh sensor data
-// setInterval(function(){
-//   console.log(imu.read());
-// },50)
-
-//////////////////////
-// HUMIDITY EXAMPLE \\
-// var humidity = matrix.humidity(); // get Humidity object
-
-// // get & refresh sensor data
-// setInterval(function(){
-//   console.log(humidity.read());
-// },50)
-
-//////////////////////
-// PRESSURE EXAMPLE \\
-// var pressure = matrix.pressure(); // get Pressure object
-
-// // get & refresh sensor data
-// setInterval(function(){
-//   console.log(pressure.read());
-// },50)
-
-////////////////
-// UV EXAMPLE \\
-// var uv = matrix.uv(); // get UV object
-
-// // get & refresh sensor data
-// setInterval(function(){
-//   console.log(uv.read());
-// },50)
+module.exports = exported();
