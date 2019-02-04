bfe9dd18c57827d537042911c82c7102766da674
Calos Chacin <chacin.carlos9798@gmail.com>
2019-02-04 13:34:26 -0500

    Added GPIO functionality

--- hal_wrapper/drivers/gpio.cpp
+++ hal_wrapper/drivers/gpio.cpp
@@ -3,12 +3,10 @@
 #include "gpio.h"
 #include "matrix_hal/gpio_control.h"
 #include <v8.h>
+#include <iostream>
 
 // Create GPIOControl object
 matrix_hal::GPIOControl gpio_control;
-
-////////////////////////
-// PIN CONFIG METHODS//
 
 // - Set GPIO Pin state (digital)
 // Paramters: int pin (0-15), int mode (0=INPUT or 1=OUTPUT)
@@ -41,9 +39,6 @@ NAN_METHOD(SetFunction){
     gpio_control.SetFunction(pin, pinFunction);
 }
 
-//////////////////////////
-// PWM GET/SET METHODS //
-
 // - Set GPIO Pin PWM
 // Parameters: float frequency, float percentage, int pin (0-15)
 NAN_METHOD(SetPWM){
@@ -51,46 +46,51 @@ NAN_METHOD(SetPWM){
     int args = 3;
     for(int i = 0; i < args; i++){
         // if argument is not a number, throw error
-        if (!info[i]->IsNumber()){Nan::ThrowTypeError("Arguments 1 & 2 must be a number");return;}
+        if (!info[i]->IsNumber()){Nan::ThrowTypeError("Arguments must be a number");return;}
     }  
 
     // get user arguments
-    int frequency = info[0]->NumberValue();
-    int percentage = info[1]->NumberValue();
-    int pin = info[2]->NumberValue();
+    float frequency = info[0]->NumberValue();
+    float percentage = info[1]->NumberValue();
+    uint16_t pin = info[2]->NumberValue();
+
+    // std::cout << "frequency: " << frequency << std::endl;
+    // std::cout << "percentage: " << percentage << std::endl;
+    // std::cout << "pin: " << pin << std::endl;
 
     // set PWM output
     gpio_control.SetPWM(frequency, percentage, pin);
 }
 
-// - Set A Servo Angle from PWM
-// Parameters: float angle, float min_pulse_ms, int pin (0-15)
+// - Set Servo angle through PWM
+// Parameters: float frequency, float percentage, int pin (0-15)
 NAN_METHOD(SetServoAngle){
     // for each required argument
     int args = 3;
     for(int i = 0; i < args; i++){
         // if argument is not a number, throw error
-        if (!info[i]->IsNumber()){Nan::ThrowTypeError("Arguments 1, 2, & 3 must be a number");return;}
-    }
+        if (!info[i]->IsNumber()){Nan::ThrowTypeError("Arguments must be a number");return;}
+    }  
 
     // get user arguments
-    int angle = info[0]->NumberValue();
-    int min_pulse_ms = info[1]->NumberValue();
-    int pin = info[2]->NumberValue();
+    float angle = info[0]->NumberValue();
+    float min_pulse_ms = info[1]->NumberValue();
+    uint16_t pin = info[2]->NumberValue();
 
-    // set servo angle
+    // std::cout << "pin: " << pin << std::endl;
+    // std::cout << "angle: " << angle << std::endl;
+    // std::cout << "min_pulse_ms: " << min_pulse_ms << std::endl;
+
+    // set PWM output
     gpio_control.SetServoAngle(angle, min_pulse_ms, pin);
 }
 
-//////////////////////////////
-// DIGITAL GET/SET METHODS //
-
-// - Read GPIO Pin state (digital)
-NAN_METHOD(ReadDigital){
+// - Read GPIO Pin state (digital only)
+NAN_METHOD(GetValue){
     // if first argument is not a number, throw error
     if (!info[0]->IsNumber()) {Nan::ThrowTypeError("Argument must be a number");return;}
     // get user arguments
-    bool pinValue = gpio_control.GetGPIOValue(info[0]->NumberValue());
+    uint16_t pinValue = gpio_control.GetGPIOValue(info[0]->NumberValue());
     // return desired pin's value
     info.GetReturnValue().Set(pinValue);
 }
@@ -106,6 +106,7 @@ NAN_METHOD(SetDigital){
 
     // if argument 2 is not 0 (OFF) or 1 (ON), throw error
     if (digitalState != 1 && digitalState != 0){Nan::ThrowTypeError("Argument 2 must be 0 or 1");return;}
+    
     // set pin to desired value
     gpio_control.SetGPIOValue(pin, digitalState);
 }
@@ -126,19 +127,20 @@ NAN_METHOD(gpio) {
     Nan::Set(obj, Nan::New("setFunction").ToLocalChecked(),
     Nan::GetFunction(Nan::New<v8::FunctionTemplate>(SetFunction)).ToLocalChecked());
     
-    // Digital read method
-    Nan::Set(obj, Nan::New("readDigital").ToLocalChecked(),
-    Nan::GetFunction(Nan::New<v8::FunctionTemplate>(ReadDigital)).ToLocalChecked());
-    // Digital set method
+    // read method (Digital only)
+    Nan::Set(obj, Nan::New("getValue").ToLocalChecked(),
+    Nan::GetFunction(Nan::New<v8::FunctionTemplate>(GetValue)).ToLocalChecked());
+    // set method (Digital only)
     Nan::Set(obj, Nan::New("setDigital").ToLocalChecked(),
     Nan::GetFunction(Nan::New<v8::FunctionTemplate>(SetDigital)).ToLocalChecked());
 
-    // PWM set servo angle method
-    Nan::Set(obj, Nan::New("setServoAngle").ToLocalChecked(),
-    Nan::GetFunction(Nan::New<v8::FunctionTemplate>(SetServoAngle)).ToLocalChecked());
     // PWM set output method
     Nan::Set(obj, Nan::New("setPWM").ToLocalChecked(),
     Nan::GetFunction(Nan::New<v8::FunctionTemplate>(SetPWM)).ToLocalChecked());
+
+    // PWM set output method
+    Nan::Set(obj, Nan::New("setServoAngle").ToLocalChecked(),
+    Nan::GetFunction(Nan::New<v8::FunctionTemplate>(SetServoAngle)).ToLocalChecked());
 
     // Return object
     info.GetReturnValue().Set(obj);
